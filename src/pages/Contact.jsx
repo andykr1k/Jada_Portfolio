7cf3a8e28ff4d83259e042e44ed08f749601ee73
andykr1k <69021102+andykr1k@users.noreply.github.com>
2023-07-20 11:59:00 -0700

    Changed Styles

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,16 +2,16 @@ import { motion } from "framer-motion"
 
 function Contact() {
   return (
-    <div id="contact" className="grid place-items-center space-y-5 p-5 pb-32 h-screen">
-      <h1 className='pb-5 font-bold text-3xl flex justify-center'>Contact</h1>
+    <div id="contact" className="grid place-items-center space-y-5 p-5 h-screen">
         <div class="mx-auto max-w-screen-xl">
+        <h1 className='pb-10 font-bold text-3xl flex justify-center'>Contact Me!</h1>
 
             <div class="rounded-lg bg-white p-8 shadow-lg lg:col-span-3 lg:p-12">
                 <form action="" class="space-y-4">
                 <div>
                     <label class="sr-only" for="name">Name</label>
                     <input
-                    class="w-full rounded-lg border-gray-200 p-3 text-sm"
+                    class="w-full rounded-lg border-gray-200 p-3 text-sm bg-transparent outline"
                     placeholder="Name"
                     type="text"
                     id="name"
@@ -22,7 +22,7 @@ function Contact() {
                     <div>
                     <label class="sr-only" for="email">Email</label>
                     <input
-                        class="w-full rounded-lg border-gray-200 p-3 text-sm"
+                        class="w-full rounded-lg border-gray-200 p-3 text-sm bg-transparent outline"
                         placeholder="Email address"
                         type="email"
                         id="email"
@@ -32,7 +32,7 @@ function Contact() {
                     <div>
                     <label class="sr-only" for="phone">Phone</label>
                     <input
-                        class="w-full rounded-lg border-gray-200 p-3 text-sm"
+                        class="w-full rounded-lg border-gray-200 p-3 text-sm bg-transparent outline"
                         placeholder="Phone Number"
                         type="tel"
                         id="phone"
@@ -44,7 +44,7 @@ function Contact() {
                     <label class="sr-only" for="message">Message</label>
 
                     <textarea
-                    class="w-full rounded-lg border-gray-200 p-3 text-sm"
+                    class="w-full rounded-lg border-gray-200 p-3 text-sm bg-transparent outline"
                     placeholder="Message"
                     rows="8"
                     id="message"
@@ -54,7 +54,7 @@ function Contact() {
                 <div class="mt-4">
                     <button
                     type="submit"
-                    class="inline-block w-full rounded-lg bg-black px-5 py-3 font-medium text-white sm:w-auto"
+                    class="inline-block w-full rounded-lg btn btn-primary px-5 py-3 font-medium text-white sm:w-auto"
                     >
                     Send Enquiry
                     </button>
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -9,8 +9,8 @@ function Profile() {
             </div>
             <div className="grid place-items-center">
                 <div className="space-y-3 p-6">
-                    <h1 className="flex justify-center text-center text-3xl">Jada Ganim</h1>
-                    <h2 className="flex justify-center text-center text-xl">Director - Writer - Producer</h2>
+                    <h1 className="flex justify-center text-center text-3xl font-bold">Jada Ganim</h1>
+                    <h2 className="flex justify-center text-center text-xl font-semibold">Director - Writer - Producer</h2>
                     <h3 className="flex items-center text-center text-lg">I am a second generation Middle Eastern-American woman and passionate storyteller. Having grown up using movies as a place to feel seen and less alone, I have developed a want to share diverse stories of people who come from all walks of life through their own experiences, dreams, culture and history. Fall 2023, I will be attending Emerson College in a low-residency Writing for Film and Television Graduate Program</h3>
                 </div>
             </div>
