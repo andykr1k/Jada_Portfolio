39055f359f01149af61a481954b9b3efa862f4a9
andykr1k <69021102+andykr1k@users.noreply.github.com>
2023-08-07 09:27:14 -0700

    Changed Contact Section

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -8,8 +8,8 @@ function Contact() {
         <div className="mx-auto max-w-screen-xl">
         <h1 className='pb-10 font-bold text-3xl flex justify-center'>Contact Me!</h1>
 
-            <div className="rounded-lg bg-white p-8 shadow-lg lg:col-span-3 lg:p-12">
-              <div className="">
+            <div className="rounded-lg bg-white p-8 shadow-lg lg:col-span-3 lg:p-12 justify-center space-y-2">
+              <div className="flex justify-center">
                     <a
                     type="submit"
                     href="sms:+16502889490"
@@ -18,7 +18,7 @@ function Contact() {
                     Send Text
                     </a>
                 </div>
-                <div className="">
+                <div className="flex justify-center">
                     <a
                     type="submit"
                     href="mailto:jadaganim@gmail.com"
